5f638f256a8996c988d6e23a6da0a3dc964bff54
YukihiraDeve <valentinbourpro@gmail.com>
2023-11-09 17:46:58 +0100

    feat: + et - ajouter dans le panier + bouton retour

--- frontend/src/components/CartContext.js
+++ frontend/src/components/CartContext.js
@@ -13,11 +13,25 @@ export const CartProvider = ({ children }) => {
     }, [cartItems]);
 
     const addToCart = (product) => {
-        setCartItems([...cartItems, product]);
+        // Vérifier si le produit est déjà dans le panier
+        const productIndex = cartItems.findIndex((item) => item.id === product.id);
+
+        if (productIndex !== -1) {
+            // Si le produit est déjà dans le panier, mettre à jour sa quantité
+            const newCartItems = [...cartItems];
+            newCartItems[productIndex] = {
+                ...newCartItems[productIndex],
+                quantity: newCartItems[productIndex].quantity + 1,
+            };
+            setCartItems(newCartItems);
+        } else {
+            // Si le produit n'est pas dans le panier, l'ajouter avec une quantité de 1
+            setCartItems([...cartItems, { ...product, quantity: 1 }]);
+        }
     };
 
     return (
-        <CartContext.Provider value={{ cartItems, addToCart }}>
+        <CartContext.Provider value={{ cartItems, setCartItems,addToCart }}>
             {children}
         </CartContext.Provider>
     );
--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -2,35 +2,64 @@ import '../styles/main.css';
 import '../styles/CartPage.css'
 import React, { useContext } from 'react';
 import { CartContext } from '../components/CartContext';
-import '../styles/CartPage.css'; // Assure-toi que ce fichier contient tous les styles nécessaires
+import { useNavigate } from 'react-router-dom';
 
 const CartPage = () => {
     const { cartItems, setCartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
 
     const updateQuantity = (index, newQuantity) => {
-        const newCartItems = [...cartItems];
-        newCartItems[index].quantity = newQuantity;
-        setCartItems(newCartItems);
+        if (newQuantity < 1) {
+            // Si la quantité est inférieure à 1, supprimer l'article du panier
+            const newCartItems = [...cartItems];
+            newCartItems.splice(index, 1); // Supprime l'article à l'index spécifié
+            setCartItems(newCartItems);
+        } else {
+            // Sinon, mettre à jour la quantité de l'article
+            const newCartItems = [...cartItems];
+            newCartItems[index] = {
+                ...newCartItems[index],
+                quantity: newQuantity
+            };
+            setCartItems(newCartItems);
+        }
+    };
+
+    const goToProductsPage = () => {
+        navigate('/product'); // Utilise la méthode push pour naviguer vers la page des produits
+    };
+
+
+    const handleDecreaseQuantity = (index) => {
+        updateQuantity(index, cartItems[index].quantity - 1);
+    };
+
+    const handleIncreaseQuantity = (index) => {
+        updateQuantity(index, cartItems[index].quantity + 1);
     };
 
     return (
         <div className="background">
             <div className="container">
                 <h1 className="title">Votre Panier</h1>
-                <div class="cart-items-container">
+                <div className="cart-items-container">
                     {cartItems.map((item, index) => (
-                        <div className="cart-item" key={index}>
+                        <div className="cart-item" key={item.id}>
                             <span className="item-name">{item.name}</span>
                             <div className="quantity-controls">
-                                <button onClick={() => updateQuantity(index, item.quantity - 1)}>-</button>
+                                <button onClick={() => handleDecreaseQuantity(index)}>-</button>
                                 <span>{item.quantity}</span>
-                                <button onClick={() => updateQuantity(index, item.quantity + 1)}>+</button>
+                                <button onClick={() => handleIncreaseQuantity(index)}>+</button>
                             </div>
-                            <span className="item-price">{item.price} €</span>
+                            <span className="item-price">{(item.price * item.quantity).toFixed(2)} €</span>
                         </div>
                     ))}
                 </div>
                 <button className="place-order-button">Commander</button>
+                <button className="back-to-products-button" onClick={goToProductsPage}>
+                    Retour aux articles
+                </button>
             </div>
         </div>
     );
