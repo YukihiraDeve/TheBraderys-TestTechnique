c83d4c992035bff8159f71185802221110b4e987
YukihiraDeve <valentinbourpro@gmail.com>
2023-11-09 17:16:34 +0100

    feat: Panier avec defilement fait

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,23 +1,35 @@
 import '../styles/main.css';
 import '../styles/CartPage.css'
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import { CartContext } from '../components/CartContext';
+import '../styles/CartPage.css'; // Assure-toi que ce fichier contient tous les styles nécessaires
 
 const CartPage = () => {
-    const { cartItems } = useContext(CartContext);
-    console.log("Articles dans le panier:", cartItems);
+    const { cartItems, setCartItems } = useContext(CartContext);
+
+    const updateQuantity = (index, newQuantity) => {
+        const newCartItems = [...cartItems];
+        newCartItems[index].quantity = newQuantity;
+        setCartItems(newCartItems);
+    };
 
     return (
         <div className="background">
             <div className="container">
                 <h1 className="title">Votre Panier</h1>
-                <ul>
+                <div class="cart-items-container">
                     {cartItems.map((item, index) => (
-                        <li key={index}>
-                            {item.name} - {item.price} € - Quantité: {item.quantity || 1}
-                        </li>
+                        <div className="cart-item" key={index}>
+                            <span className="item-name">{item.name}</span>
+                            <div className="quantity-controls">
+                                <button onClick={() => updateQuantity(index, item.quantity - 1)}>-</button>
+                                <span>{item.quantity}</span>
+                                <button onClick={() => updateQuantity(index, item.quantity + 1)}>+</button>
+                            </div>
+                            <span className="item-price">{item.price} €</span>
+                        </div>
                     ))}
-                </ul>
+                </div>
                 <button className="place-order-button">Commander</button>
             </div>
         </div>
